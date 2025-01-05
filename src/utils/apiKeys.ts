bb1ee6683cfd18e11f37a6e6f5ae759b2a0f953d
redzOOO <leeredz@yahoo.com>
2025-01-05 23:01:24 +0000

    Update apiKeys.ts

--- src/utils/apiKeys.ts
+++ src/utils/apiKeys.ts
@@ -1,5 +1,5 @@
 // Utility functions for managing API keys securely
-const API_KEY_STORAGE_KEY = 'dvla_api_key';
+const API_KEY_STORAGE_KEY = 'DVLA_API_KEY';
 
 export const getDvlaApiKey = () => {
   return localStorage.getItem(API_KEY_STORAGE_KEY);
